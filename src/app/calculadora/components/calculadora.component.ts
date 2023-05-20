664946436bb1de788089e942079dcebd5b26f71a
aires1294 <pedrotja.engenharia@gmail.com>
2023-05-19 23:20:34 -0300

    add componentes

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -7,5 +7,11 @@ import { CalculadoraService } from '../services';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent {
+
+  private numero1: string;
+  private numero2: string;
+  private resultado: number;
+  private operacao: string;
+
   constructor(private calculadoraService: CalculadoraService) {}
 }
