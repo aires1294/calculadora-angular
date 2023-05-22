ad89a6e46820687f2d023e897bf6f6a9d1986dfa
aires1294 <pedrotja.engenharia@gmail.com>
2023-05-22 15:55:38 -0300

    add funcoes

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CalculadoraService } from '../services';
 
 @Component({
@@ -6,7 +6,7 @@ import { CalculadoraService } from '../services';
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
 })
-export class CalculadoraComponent {
+export class CalculadoraComponent implements OnInit {
 
   private numero1: string;
   private numero2: string;
@@ -14,4 +14,39 @@ export class CalculadoraComponent {
   private operacao: string;
 
   constructor(private calculadoraService: CalculadoraService) {}
+
+  ngOnInit(): void {
+      this.limpar();
+  }
+
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = null;
+    this.resultado = null;
+    this.operacao = null;
+  }
+
+  adicionarNumero(numero: string): void {
+    if (this.operacao === null) {
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    } else {
+      this.numero2 = this.concatenarNumero(this.numero2, numero);
+    }
+  }
+
+  concatenarNumero(numAtual: string, numConcat: string): string {
+    if (numAtual === '0' || numAtual === null) {
+      numAtual = '';
+    }
+
+    if(numConcat === '.' && numAtual === '') {
+      return '0.';
+    }
+
+    if (numConcat === '.' && numAtual.indexOf('.') > -1) {
+      return numAtual;
+    }
+  }
+
 }
+
