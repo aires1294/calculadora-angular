0e2ae02d69d385505667c63d59d9b8abeeb563a2
aires1294 <pedrotja.engenharia@gmail.com>
2023-05-22 16:04:05 -0300

    display

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -50,5 +50,46 @@ export class CalculadoraComponent implements OnInit {
     return numAtual + numConcat;
   }
 
+  definirOperacao(operacao: string): void {
+    if (this.operacao === null) {
+      this.operacao = operacao;
+      return;
+    }
+
+    if (this.numero2 !== null) {
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao);
+        this.operacao = operacao;
+        this.numero1 = this.resultado.toString();
+        this.numero2 = null;
+        this.resultado = null;
+    }
+  }
+
+  calcular(): void {
+    if (this.numero2 === null) {
+      return;
+    }
+
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.numero1),
+      parseFloat(this.numero2),
+      this.operacao);
+  }
+
+  get display(): string {
+    if (this.resultado !== null) {
+      return this.resultado.toString();
+    }
+
+    if (this.numero2 !== null) {
+      return this.numero2;
+    }
+
+    return this.numero1;
+  }
+
 }
 
